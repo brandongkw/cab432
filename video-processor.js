dd65e2edbfec1b0abc6751eb73cb21f640498e01
Kai <n11529679@qut.edu.au>
2024-11-16 00:34:26 +1000

    URL changes

--- video-processor.js
+++ video-processor.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong. Please try again later.');
 });
 
-app.post('/process', async (req, res) => {
+app.post('/vs/process', async (req, res) => {
     console.log('Received request for video processing:', req.body);
     const { format, resolution, selectedVideo, userId } = req.body; // Get userId from the request body
     const localVideoPath = `./videos/${selectedVideo}`;
